88ffddfa49b209ded725dd4db05d259d21dedee1
Graham <graham.gibbons@gmail.com>
2024-02-09 11:06:23 -0800

    Connect local path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
+import glsl from "vite-plugin-glsl";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   root: "src",
+  plugins: [glsl(), tsconfigPaths()],
   build: {
     outDir: "../dist",
     sourcemap: true,
